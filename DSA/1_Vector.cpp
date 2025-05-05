2656097e380bf30c36975e74173917a54ee7f4c7
Ashitabh-Srivastava <ashitabhsrivastava@gmail.com>
2025-05-05 16:11:39 +0530

    nothing

--- DSA/1_Vector.cpp
+++ DSA/1_Vector.cpp
@@ -5,62 +5,62 @@ using namespace std;
 
 int main()
 {
-    vector<string> foods = {"Oats", "Eggs", "Lassi", "Chicken", "Milk"}; // Declaration
+     vector<string> foods = {"Oats", "Eggs", "Lassi", "Chicken", "Milk"}; // Declaration
 
-    for (string food : foods) // Print all elements
-    {
-        cout << food << "\t";
-    }
+     for (string food : foods) // print all elements
+     {
+          cout << food << "\t";
+     }
 
-    cout << endl
-         << foods[1] << "\t" << foods[3]; // access elements by Index
+     cout << endl
+          << foods[1] << "\t" << foods[3]; // access elements by Index
 
-    cout << "\n\n";
+     cout << "\n\n";
 
-    // vector Functions
+     // vector Functions
 
-    cout << foods.at(2); /* also access by Index, but safer as shows error in out of range
-   << foods[5] << endl << foods.at(5) << endl;*/
+     cout << foods.at(2); /* also access by Index, but safer as shows error in out of range
+    << foods[5] << endl << foods.at(5) << endl;*/
 
-    foods.at(2) = "Paneer"; // changing values
-    cout << endl
-         << foods.at(2);
+     foods.at(2) = "Paneer"; // changing values
+     cout << endl
+          << foods.at(2);
 
-    cout << "\n\n";
+     cout << "\n\n";
 
-    cout << foods.front()         // gives First element
-         << "\t" << foods.back(); // gives Last element
+     cout << foods.front()         // gives First element
+          << "\t" << foods.back(); // gives Last element
 
-    cout << "\n\n";
+     cout << "\n\n";
 
-    foods.push_back("Peanuts"); // add elements at end
-    foods.push_back("Banana");
+     foods.push_back("Peanuts"); // add elements at end
+     foods.push_back("Banana");
 
-    for (string food : foods) // Print all elements
-    {
-        cout << food << "\t";
-    }
+     for (string food : foods) // Print all elements
+     {
+          cout << food << "\t";
+     }
 
-    cout << endl;
+     cout << endl;
 
-    foods.pop_back(); // removes an element from end
+     foods.pop_back(); // removes an element from end
 
-    for (string food : foods) // Print all elements
-    {
-        cout << food << "\t";
-    }
+     for (string food : foods) // Print all elements
+     {
+          cout << food << "\t";
+     }
 
-    cout << "\n\n";
+     cout << "\n\n";
 
-    vector<int> nums;
+     vector<int> nums;
 
-    cout << nums.size()           // gives the vector Size
-         << "\t" << nums.empty(); // tells vector has elements = 1 OR not = 0
+     cout << nums.size()           // gives the vector Size
+          << "\t" << nums.empty(); // tells vector has elements = 1 OR not = 0
 
-    cout << endl
-         << foods.size() << "\t" << foods.empty();
+     cout << endl
+          << foods.size() << "\t" << foods.empty();
 
-    cout << "\n\n";
+     cout << "\n\n";
 
-    return 0;
+     return 0;
 }
--- DSA/2_List.cpp
+++ DSA/2_List.cpp
@@ -3,7 +3,60 @@
 #include <list>
 using namespace std;
 
-int main(){
+int main()
+{
+
+    list<int> todo = {20, 3, 4, 5, 6, 7, 8, 90}; // Declaration
+
+    for (int i : todo) // print all elements
+    {
+        cout << i << "\t";
+    }
+
+    cout << "\n\n";
+
+    // list Functions
+
+    cout << todo.front() << "\t" << todo.back(); // gives First and Last element
+
+    cout << endl;
+
+    todo.front() = 2; // changing values
+    todo.back() = 9;
+
+    cout << todo.front() << "\t" << todo.back();
+
+    cout << "\n\n";
+
+    todo.push_front(1); // add elements at front
+    todo.push_front(0);
+
+    todo.push_back(10); // add elements at end
+    todo.push_back(11);
+
+    todo.pop_front(); // removes an element from front = 0
+    todo.pop_back();  // removes an element from end = 11
+
+    for (int i : todo)
+    {
+        cout << i << "\t";
+    }
+
+    cout << "\n\n";
+
+    list<string> texts;
+
+    cout << texts.size()           // gives the list Size
+         << "\t" << texts.empty(); // tells list has elements = 1 OR not = 0
+
+    cout << endl;
+
+    cout << todo.size()
+         << "\t" << todo.empty();
+
+    cout << "\n\n";
+
+    
 
     return 0;
 }
