f8bb308c7a70f36e6031aeb701654ccf6e8c0c99
Ashitabh-Srivastava <ashitabhsrivastava@gmail.com>
2025-05-05 14:22:38 +0530

    nothing

--- DSA/1_Vectors.cpp
+++ DSA/1_Vectors.cpp
@@ -5,27 +5,64 @@ using namespace std;
 
 int main()
 {
-    vector<string> foods = {"Oats", "Eggs", "Milk", "Chicken", "Lassi"}; // Declaration
+    vector<string> foods = {"Oats", "Eggs", "Lassi", "Chicken", "Milk"}; // Declaration
 
-    for (string food : foods) // Printing all elements
+    for (string food : foods) // Print all elements
     {
-        cout << food << endl;
+        cout << food << "\t";
     }
 
     cout << endl
-         << foods[1] << "\t" << foods[3]; // accessing elements by Index
+         << foods[1] << "\t" << foods[3]; // access elements by Index
 
     cout << "\n\n";
 
-    // using Functions
+    // vector Functions
 
-    cout << foods.at(2)       // also by Index, but safer as shows error in out of range
-         << foods[5] << endl; //<< foods.at(5) << endl;
+    cout << foods.at(2); /* also access by Index, but safer as shows error in out of range
+   << foods[5] << endl << foods.at(5) << endl;*/
 
-    cout << foods.front();         // gives First element
-    cout << foods.back(); // gives Last element
+    foods.at(2) = "Paneer"; // changing values
+    cout << endl
+         << foods.at(2);
 
-    cout << "";
+    cout << "\n\n";
+
+    cout << foods.front()         // gives First element
+         << "\t" << foods.back(); // gives Last element
+
+    cout << "\n\n";
+
+    foods.push_back("Peanuts"); // add elements at end
+    foods.push_back("Banana");
+
+    for (string food : foods) // Print all elements
+    {
+        cout << food << "\t";
+    }
+
+    cout << endl;
+
+    foods.pop_back(); // removes an element from end
+
+    for (string food : foods) // Print all elements
+    {
+        cout << food << "\t";
+    }
+
+    cout << "\n\n";
+
+    vector<int> nums;
+
+    cout << nums.size()           // gives the vector Size
+         << "\t" << nums.empty(); // tells vector has elements = 1 OR not = 0
+
+    cout << endl
+         << foods.size() << "\t" << foods.empty();
+
+    cout << "\n\n";
+
+    
 
     return 0;
 }
