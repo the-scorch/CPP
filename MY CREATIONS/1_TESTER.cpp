aa73b599d53a7d1aaea42f78fa1efba37c9770d9
Ashitabh-Srivastava <ashitabhsrivastava@gmail.com>
2025-06-16 21:34:51 +0530

    nothing

--- MY CREATIONS/1_TESTER.cpp
+++ MY CREATIONS/1_TESTER.cpp
@@ -1,38 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-   
-   int r1, r2;
-   cin >> r1 >> r2;
-   bool flag = true;
-
-   for(int i = r1; i <= r2; i++)
-   {
-      if(flag==true){
-         if(i==0 || i==1)
-   {
-      cout << "1 is not a prime number." << endl;
-      flag = false;
-   }
-      }
-      int count = 0;
-      for(int j = 2; j*j <= i; j++)
-      {
-         if(i % j == 0)
-         {
-            count++;
-            break;
-         }
-      }
-      if(i > 1)
-      {
-      if(count == 0)
-      {
-         cout << i << " ";
-      }
-   }
-   }
-
-return 0;
+int main()
+{
+   return 0;
 }
